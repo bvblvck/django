f23c7507687336f1712ad88e4308a516a8ac1480
bvblvck <bvblvck@gmail.com>
2022-06-18 12:41:22 -0400

    more changes.

--- I4G015652UGS/blog/models.py
+++ I4G015652UGS/blog/models.py
@@ -7,6 +7,6 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(auto_now_add=True)
     author = models.TextField()
     published_date = models.DateTimeField()
+    created_date = models.DateTimeField()
